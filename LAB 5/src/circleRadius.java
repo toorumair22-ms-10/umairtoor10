6a2f212d63262fa27c9a5d658f571ae5f1bed08c
toorumair22-ms-10 <toorumair22@gmail.com>
2026-05-29 11:24:41 +0500

    lab 5 uploaded

---
+++ LAB 5/src/circleRadius.java
@@ -0,0 +1,9 @@
+public class circleRadius {
+    public static void main(String[] args) {
+        double rad = 3442.23;
+        double computedArea = Math.PI * rad * rad;
+
+        System.out.println("Computed Circle Area: " + computedArea);
+        System.out.println("Circle Area (as Integer): " + (int) computedArea);
+    }
+}
