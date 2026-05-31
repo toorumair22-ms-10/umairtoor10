e3f4c3c34e0a4b8868773681a332a6876070cd9a
toorumair22-ms-10 <toorumair22@gmail.com>
2026-05-31 17:58:54 +0500

    Add files via upload

---
+++ INHERITENCE lab 8/LAB 8/IN1.java
@@ -0,0 +1,12 @@
+package INHERITENCE;
+
+public class IN1 {
+    public static void main(String[]args){
+    measurements m1=new box();
+    box b1=new box();
+    //m1.dis();  not allowed by m1 we can only access the methods of measurements
+
+
+
+    }
+}
